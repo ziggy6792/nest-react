ea2d82ea8b92de9ced86bad3ea0cfa72eee63655
Simon <simonaverhoeven@gmail.com>
2025-11-15 15:29:57 +0700

    upgrade everything

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -1,11 +1,13 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vitest/config";
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+  plugins: [react()],
   test: {
     globals: true,
-    environment: "jsdom",
-    setupFiles: ["./src/test-setup.ts"],
-    include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    environment: 'jsdom',
+    setupFiles: ['./src/test-setup.ts'],
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
   },
 });
