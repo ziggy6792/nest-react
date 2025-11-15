064476dc1093d18005f1db7146c66b810a71ec41
Simon <simonaverhoeven@gmail.com>
2025-11-15 16:08:09 +0700

    add local lint config

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,4 +1,19 @@
-/** @type {import("eslint").Linter.Config} */
-import { config } from "@repo/eslint-config";
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
-export default config;
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    ignores: ['src/api/generated/**'],
+    extends: [js.configs.recommended, tseslint.configs.recommended, reactHooks.configs.flat.recommended, reactRefresh.configs.vite],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+  },
+]);
