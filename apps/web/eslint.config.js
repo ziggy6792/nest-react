9cabbc1283771db038c836ee6f21c20658b9d5c1
Simon <simonaverhoeven@gmail.com>
2025-11-17 15:20:21 +0700

    fix lint

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -14,6 +14,9 @@ export default defineConfig([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
   },
 ]);
