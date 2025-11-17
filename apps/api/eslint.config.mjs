d0aa0ead2a537721fdb9792f2e7f30d41dfddfbc
Simon <simonaverhoeven@gmail.com>
2025-11-17 09:55:29 +0700

    separate eslint configs

--- apps/api/eslint.config.mjs
+++ apps/api/eslint.config.mjs
@@ -1,9 +1,35 @@
-import { nestJsConfig } from '@repo/eslint-config/nest-js';
+// @ts-check
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
-/** @type {import("eslint").Linter.Config} */
-export default [
-  ...nestJsConfig,
+export default tseslint.config(
   {
-    ignores: ['.prettierrc.mjs', 'eslint.config.mjs'],
+    ignores: ['eslint.config.mjs'],
   },
-];
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: 'commonjs',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    },
+  },
+);
