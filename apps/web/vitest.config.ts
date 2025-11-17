eef96ad9113b52c5e5ad015066f78c2279bad88c
Simon <simonaverhoeven@gmail.com>
2025-11-17 17:21:35 +0700

    fix web test config

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -1,17 +1,19 @@
 /// <reference types="vitest" />
-import { defineConfig, mergeConfig } from 'vitest/config';
-import viteConfig from './vite.config';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
+import { fileURLToPath } from 'url';
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    plugins: [react()],
-    test: {
-      globals: true,
-      environment: 'jsdom',
-      setupFiles: ['./src/test-setup.ts'],
-      include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      src: fileURLToPath(new URL('./src', import.meta.url)),
     },
-  })
-);
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/test-setup.ts'],
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+  },
+});
